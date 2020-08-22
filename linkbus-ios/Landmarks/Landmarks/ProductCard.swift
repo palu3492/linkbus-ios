1a5730f3ef4278991349eb967971b6d61c0307e3
Michael Carroll <mcarroll001@csbsju.edu>
2020-08-22 17:40:43 -0600

    added Landmarks app as base

---
+++ linkbus-ios/Landmarks/Landmarks/ProductCard.swift
@@ -0,0 +1,175 @@
+//
+//  ProductCard.swift
+//  FoodProductCard
+//
+//  Created by Jean-Marc Boullianne on 11/17/19.
+//  Copyright © 2019 Jean-Marc Boullianne. All rights reserved.
+//
+import SwiftUI
+
+struct ProductCard: View {
+    
+    var image:Image     // Featured Image
+    var price:Double    // USD
+    var title:String    // Product Title
+    var description:String // Product Description
+    var ingredientCount:Int // # of Ingredients
+    var peopleCount:Int     // Servings
+    var category:String?    // Optional Category
+    var buttonHandler: (()->())?
+    
+    init(title:String, description:String, image:Image, price:Double, peopleCount:Int, ingredientCount:Int, category:String?, buttonHandler: (()->())?) {
+        
+        self.title = title
+        self.description = description
+        self.image = image
+        self.price = price
+        self.peopleCount = peopleCount
+        self.ingredientCount = ingredientCount
+        self.category = category
+        self.buttonHandler = buttonHandler
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            
+            // Main Featured Image - Upper Half of Card
+            MapView(coordinate: )
+                .scaledToFill()
+                .frame(minWidth: nil, idealWidth: nil, maxWidth: UIScreen.main.bounds.width, minHeight: nil, idealHeight: nil, maxHeight: 300, alignment: .center)
+                .clipped()
+                .overlay(
+                    Text("Face Mask Required")
+                        .fontWeight(Font.Weight.medium)
+                        .font(Font.system(size: 16))
+                        .foregroundColor(Color.white)
+                        .padding([.leading, .trailing], 16)
+                        .padding([.top, .bottom], 8)
+                        .background(Color.black.opacity(0.5))
+                        .mask(RoundedCorners(tl: 0, tr: 0, bl: 0, br: 15))
+                    , alignment: .topLeading)
+            
+            // Stack bottom half of card
+            VStack(alignment: .leading, spacing: 6) {
+                Text(self.title)
+                    .fontWeight(Font.Weight.heavy)
+                Text(self.description)
+                    .font(Font.custom("HelveticaNeue-Bold", size: 16))
+                    .foregroundColor(Color.gray)
+                
+                // Horizontal Line separating details and price
+                Rectangle()
+                    .foregroundColor(Color.gray.opacity(0.3))
+                    .frame(width: nil, height: 1, alignment: .center)
+                    .padding([.leading, .trailing], -12)
+                
+                // 'Based on:' Horizontal Category Stack
+                HStack(alignment: .center, spacing: 6) {
+                    
+                    if category != nil {
+                        Text("Next bus:")
+                            .font(Font.system(size: 13))
+                            .fontWeight(Font.Weight.heavy)
+                        HStack {
+                            Text(category!)
+                            .font(Font.custom("HelveticaNeue-Medium", size: 12))
+                                .padding([.leading, .trailing], 10)
+                                .padding([.top, .bottom], 5)
+                            .foregroundColor(Color.white)
+                        }
+                        .background(Color(red: 43/255, green: 175/255, blue: 187/255))
+                        .cornerRadius(7)
+                        Spacer()
+                    }
+                    
+                    HStack(alignment: .center, spacing: 0) {
+                        Text("Face")
+                            .foregroundColor(Color.gray)
+                        Text("\(self.ingredientCount)")
+                    }.font(Font.custom("HelveticaNeue", size: 14))
+                    
+                }
+
+                
+                // 'Based on:' Horizontal Category Stack
+                HStack(alignment: .center, spacing: 6) {
+                    
+                    if category != nil {
+                        Text("Last bus:")
+                            .font(Font.system(size: 13))
+                            .fontWeight(Font.Weight.heavy)
+                        HStack {
+                            Text(category!)
+                            .font(Font.custom("HelveticaNeue-Medium", size: 12))
+                                .padding([.leading, .trailing], 10)
+                                .padding([.top, .bottom], 5)
+                            .foregroundColor(Color.white)
+                        }
+                        .background(Color(red: 43/255, green: 175/255, blue: 187/255))
+                        .cornerRadius(7)
+                        Spacer()
+                    }
+                    
+                    HStack(alignment: .center, spacing: 0) {
+                        Text("Face")
+                            .foregroundColor(Color.gray)
+                        Text("\(self.ingredientCount)")
+                    }.font(Font.custom("HelveticaNeue", size: 14))
+                    
+                }
+                .padding([.top, .bottom], 8)
+               
+                
+            }
+            .padding(12)
+            
+        }
+        .background(Color.white)
+        .cornerRadius(15)
+        .shadow(color: Color.black.opacity(0.2), radius: 7, x: 0, y: 2)
+    }
+}
+
+struct ProductCard_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductCard(title: "Gorecki to Sexton", description: "College of Saint Benedict", image: Image("Smoothie_Bowl"), price: 15.00, peopleCount: 2, ingredientCount: 12, category: "5 minutes", buttonHandler: nil)
+    }
+}
+
+struct RoundedCorners: Shape {
+    var tl: CGFloat = 0.0
+    var tr: CGFloat = 0.0
+    var bl: CGFloat = 0.0
+    var br: CGFloat = 0.0
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        let w = rect.size.width
+        let h = rect.size.height
+
+        let tr = min(min(self.tr, h/2), w/2)
+        let tl = min(min(self.tl, h/2), w/2)
+        let bl = min(min(self.bl, h/2), w/2)
+        let br = min(min(self.br, h/2), w/2)
+
+        path.move(to: CGPoint(x: w / 2.0, y: 0))
+        path.addLine(to: CGPoint(x: w - tr, y: 0))
+        path.addArc(center: CGPoint(x: w - tr, y: tr), radius: tr,
+                    startAngle: Angle(degrees: -90), endAngle: Angle(degrees: 0), clockwise: false)
+
+        path.addLine(to: CGPoint(x: w, y: h - br))
+        path.addArc(center: CGPoint(x: w - br, y: h - br), radius: br,
+                    startAngle: Angle(degrees: 0), endAngle: Angle(degrees: 90), clockwise: false)
+
+        path.addLine(to: CGPoint(x: bl, y: h))
+        path.addArc(center: CGPoint(x: bl, y: h - bl), radius: bl,
+                    startAngle: Angle(degrees: 90), endAngle: Angle(degrees: 180), clockwise: false)
+
+        path.addLine(to: CGPoint(x: 0, y: tl))
+        path.addArc(center: CGPoint(x: tl, y: tl), radius: tl,
+                    startAngle: Angle(degrees: 180), endAngle: Angle(degrees: 270), clockwise: false)
+
+        return path
+    }
+}
